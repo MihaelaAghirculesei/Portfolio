80e941c5d58ceb0bd1a1dd77345c4870a72eecfe
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-03-30 18:23:22 +0200

    Abut Me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { HeaderComponent } from './shared/header.component';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
 import { MainContentComponent } from './main-content/main-content.component';
 import { LandingpageComponent } from './1_landing-page/landing-page.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TranslateModule, MainContentComponent, LandingpageComponent],
+  imports: [RouterOutlet, HeaderComponent, TranslateModule, MainContentComponent, LandingpageComponent, AboutmeComponent],
   providers: [TranslateService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
