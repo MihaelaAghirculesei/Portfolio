40416d558838b8995a297e66c1e667ca0006ee37
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-07-04 20:57:53 +0200

    Feat: add animated navigation buttons to hero section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { LandingpageComponent } from '../1_landing-page/landing-page.component';
+// import { LandingpageComponent } from '../1_landing-page/landing-page.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingpageComponent,],
+  // imports: [LandingpageComponent,],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
