a541e3092ddf01cbde9e1623cb6fff889c52b6bb
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-08-23 12:45:26 +0200

    Privacy policy added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { LegalNoticeComponent } from './main-content/legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './main-content/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
-  { path: 'legal-notice', component: LegalNoticeComponent }
+  { path: 'legal-notice', component: LegalNoticeComponent },
+  { path: 'datenschutz', component: PrivacyPolicyComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent }
 ];
