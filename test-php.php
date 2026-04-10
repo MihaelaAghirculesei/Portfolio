3891154bb3773f2c613646e42c161014340e8c6c
MihaelaAghirculesei <aghirculesei@gmail.com>
2026-04-10 16:02:42 +0200

    fix: complete migration from Strato to Cloudflare Pages

--- test-php.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "PHP funziona!";
-echo "<br>POST data: ";
-print_r($_POST);
-echo "<br>Raw input: ";
-echo file_get_contents('php://input');
-echo "<br>Method: " . $_SERVER['REQUEST_METHOD'];
-?>
