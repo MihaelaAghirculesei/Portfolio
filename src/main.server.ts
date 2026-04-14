1bec663df01df4bfbf7531cdd5de4bbd92d00583
MihaelaAghirculesei <aghirculesei@gmail.com>
2026-04-14 23:21:16 +0200

    fix(ssr): update CommonEngine import and bootstrap API for v19

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,8 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-const bootstrap = (): ReturnType<typeof bootstrapApplication> => bootstrapApplication(AppComponent, appConfig);
+const bootstrap = (context: BootstrapContext): ReturnType<typeof bootstrapApplication> =>
+  bootstrapApplication(AppComponent, appConfig, context);
 
 export default bootstrap;
