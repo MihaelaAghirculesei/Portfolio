e4a5beda57f445bd8f87bb90b6517fcaa794fc08
MihaelaAghirculesei <aghirculesei@gmail.com>
2026-06-04 19:42:11 +0200

    feat(interfaces): add id field and HoverOffset interface to Projects

--- src/app/interfaces/projects.ts
+++ src/app/interfaces/projects.ts
@@ -1,12 +1,19 @@
+export interface HoverOffset {
+  base: number;
+  smallPreview: number;
+}
+
 export interface Projects {
-    name: string;
-    technologies: string [];
-    previewImg: string;
-    previewImgSrcset?: string;
-    description?: string;
-    githubUrl: string;
-    liveUrl: string;
-    isPersonal?: boolean;
-    isTeam?: boolean;
-    inProgress?: boolean;
+  id: string;
+  name: string;
+  technologies: string[];
+  previewImg: string;
+  previewImgSrcset?: string;
+  description?: string;
+  githubUrl: string;
+  liveUrl: string;
+  isPersonal?: boolean;
+  isTeam?: boolean;
+  inProgress?: boolean;
+  hoverOffset?: HoverOffset;
 }
