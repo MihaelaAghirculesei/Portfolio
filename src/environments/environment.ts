cedb66be8e3e9ee039f1441521db62ef62aa76e8
MihaelaAghirculesei <aghirculesei@gmail.com>
2026-06-04 19:42:00 +0200

    refactor(config): move project URLs from environment config to JSON data file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,27 +6,5 @@ export const environment = {
     github: 'https://github.com/MihaelaAghirculesei',
     linkedin: 'https://www.linkedin.com/in/mihaela-aghirculesei-84147a23b/'
   },
-  projects: {
-    join: {
-      github: 'https://github.com/MihaelaAghirculesei/join-kanban-board',
-      live: 'https://join-aghirculesei.pages.dev'
-    },
-    elPolloLoco: {
-      github: 'https://github.com/MihaelaAghirculesei/El-Pollo-Loco',
-      live: 'https://el-pollo-loco-aghirculesei.pages.dev'
-    },
-    pokedex: {
-      github: 'https://github.com/MihaelaAghirculesei/Pokedex',
-      live: 'https://pokedex-aghirculesei.pages.dev'
-    },
-    birthdayReminder: {
-      github: 'https://github.com/MihaelaAghirculesei/birthday-reminder-app',
-      live: 'https://birthday-reminder-aghirculesei.pages.dev'
-    },
-    todoApi: {
-      github: 'https://github.com/MihaelaAghirculesei/todo-platform',
-      live: 'https://todo-frontend-aghirculesei.onrender.com'
-    }
-  },
   enableLogging: true,
 };
